08aeb0bda80c539c4e38659d51ddcff8a9c82570
Samuel <sam.amistoso@gmail.com>
2018-05-01 14:58:06 +1200

    Completed the Mobile Menu App

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -4,12 +4,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
         //Make gulp continue even error in styles and show error message
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
